9a56bd108be5ce02577c80356a0fba5da521ce5d
keecee <keecee_a@yahoo.com>
2019-09-19 15:29:43 +0100

    commit

---
+++ src/Q5.java
@@ -0,0 +1,50 @@
+
+import javax.swing.*;
+
+public class Q5 {
+    public static void main(String []args)
+    {
+      float result;
+
+      //print your input dialog box
+
+      String choice = JOptionPane.showInputDialog(null, "A (for addition)" +
+                                                  "\n S (for subtraction)" +
+                                                   "\n M (for multiplication)" +
+                                                   "\n D (for division)" +
+                                                   "\n\nwhat Operation do you want to perform:\n",
+                                                    "Cal", JOptionPane.INFORMATION_MESSAGE);
+      Float num = Float.parseFloat(JOptionPane.showInputDialog("enter a number ;\n"));
+
+      Float num2 = Float.parseFloat(JOptionPane.showInputDialog("enter another number ; \n"));
+
+      switch (choice)
+      {
+          case "A":
+          result = num + num2;
+          JOptionPane.showMessageDialog(null, "Result of Operation is : " + result, "Addition", JOptionPane.INFORMATION_MESSAGE);
+          break;
+
+          case "S":
+              result = num - num2;
+              JOptionPane.showMessageDialog(null, "Result of Operation is : " + result, "Addition", JOptionPane.INFORMATION_MESSAGE);
+              break;
+
+          case "D":
+              result = num / num2;
+              JOptionPane.showMessageDialog(null, "Result of Operation is : " + result, "Addition", JOptionPane.INFORMATION_MESSAGE);
+              break;
+
+          case "M":
+              result = num * num2;
+              JOptionPane.showMessageDialog(null, "Result of Operation is : " + result, "Addition", JOptionPane.INFORMATION_MESSAGE);
+              break;
+
+
+          default: JOptionPane.showMessageDialog(null,"Error");
+
+
+      }
+      System.exit(0);
+    }
+}
